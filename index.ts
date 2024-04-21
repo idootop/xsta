22e1af0013f1e522a109dc7c029e707562e1e1ca
WJG <wjg12341234@163.com>
2024-04-21 17:14:27 +0800

    refactor: rebuild components after state removal

--- index.ts
+++ index.ts
@@ -1,13 +1,13 @@
-import { useEffect, useRef, useState, ReactNode } from "react";
-import { shallowEqual } from "external-shallow-equal";
+import { useEffect, useRef, useState, ReactNode } from 'react';
+import { shallowEqual } from 'external-shallow-equal';
 
-type StateRef = { state?: any; rebuilds: Map<string, VoidFunction> };
-const kStates = new Map<string, StateRef>();
+type StateContainer = { state?: any; rebuilds: Map<string, VoidFunction> };
+const kStates = new Map<string, StateContainer>();
 
 class _XSta {
   set<S = any>(key: string, nextState: SetXStateAction<S>) {
     let needRefresh = false;
-    if (typeof nextState === "function") {
+    if (typeof nextState === 'function') {
       const prevState = XSta.get(key);
       nextState = (nextState as CallableFunction)(prevState);
       if (nextState === undefined) {
@@ -15,116 +15,104 @@ class _XSta {
         nextState = prevState;
       }
     }
-    const currentStateRef = kStates.get(key);
-    if (currentStateRef) {
-      if (needRefresh || !shallowEqual(currentStateRef.state, nextState)) {
-        currentStateRef.state = nextState;
-        currentStateRef.rebuilds.forEach((rebuild) => rebuild());
+    const currentStateContainer = kStates.get(key);
+    if (currentStateContainer) {
+      if (needRefresh || !shallowEqual(currentStateContainer.state, nextState)) {
+        currentStateContainer.state = nextState;
+        currentStateContainer.rebuilds.forEach(rebuild => rebuild());
       }
     } else {
       kStates.set(key, { state: nextState, rebuilds: new Map() });
     }
   }
-  get = <S = any>(key: string): S => kStates.get(key)?.state;
-  delete = (key: string) => kStates.delete(key);
-  clear = () => kStates.clear();
+  get<S = any>(key: string, initialState?: InitialState<S>): S {
+    return kStates.get(key)?.state ?? getValue(initialState);
+  }
+  delete(key: string) {
+    const rebuilds = Array.from(kStates.get(key)?.rebuilds?.values() ?? []);
+    kStates.delete(key);
+    rebuilds.forEach(rebuild => rebuild());
+  }
+  clear() {
+    const rebuilds = Array.from(kStates.values()).reduce((pre, e) => {
+      return pre.concat(e.rebuilds.values());
+    }, [] as any[]);
+    kStates.clear();
+    rebuilds.forEach(rebuild => rebuild());
+  }
 }
 
 export const XSta = new _XSta();
 
-export type SetXStateAction<S = any> = S | ((prevState: S) => S | void);
-export type IXState<S = any> = [S, (value: SetXStateAction<S>) => void];
-export type IXStateSelector<S = any> = (state: S) => any;
-export function useXState<S = any>(
-  key: string,
-  initialState?: S | (() => S),
-  options?: { selector?: IXStateSelector<S> }
-): IXState<S> {
+type SetXStateAction<S = any> = S | ((prevState: S) => S | void);
+type XState<S = any> = [S, (value: SetXStateAction<S>) => void];
+type XStateSelector<S = any> = (state: S) => any;
+type InitialState<S = any> = S | (() => S);
+type UseStateOptions<S = any> = { selector?: XStateSelector<S> };
+export function useXState<S = any>(key: string, initialState?: InitialState<S>, options?: UseStateOptions<S>): XState<S> {
   useXProvider(key, initialState);
   const [flag, setFlag] = useState(false);
   const rebuild = () => setFlag(!flag);
   const ref = useRef({ id: `${newId()}`, rebuild });
   ref.current.rebuild = rebuild;
-  const getCurrent = (): IXState<S> => [
-    XSta.get(key),
-    (nextState) => XSta.set(key, nextState),
-  ];
-  const selectorRef = useMemoSelectorRef<IXState<S>, S>({
+  const selectorRef = useMemoWithSelector<XState<S>, S>({
     selector: options?.selector,
-    getCurrent,
-    getDeps: () => XSta.get(key),
+    getDeps: () => XSta.get(key, initialState),
     onChange: () => ref.current.rebuild(),
+    getCurrent: () => [XSta.get(key, initialState), nextState => XSta.set(key, nextState)],
   });
   useEffect(() => {
-    const id = ref.current.id;
-    registerRebuildFn(key, id, () => selectorRef.current.diffChanges());
-    return () => unregisterRebuildFn(key, id) as any;
-  }, [key, selectorRef]);
-  return selectorRef.current.state ?? getCurrent();
+    registerRebuildFn(key, ref.current.id, selectorRef.current.diffChanges);
+    return () => unregisterRebuildFn(key, ref.current.id) as any;
+  }, [rebuild]);
+  return selectorRef.current.state;
 }
 
 export abstract class XStaManager<S = any> {
   abstract key: string;
-  abstract initialState: S;
-  getState = () => XSta.get<S>(this.key) ?? this.initialState;
-  setState = (nextState: S) => XSta.set<S>(this.key, nextState);
+  abstract initialState: InitialState<S>;
+  getState = () => XSta.get<S>(this.key, this.initialState);
+  setState = (nextState: SetXStateAction<S>) => XSta.set<S>(this.key, nextState);
   deleteState = () => XSta.delete(this.key);
-  useState = (options?: { selector?: IXStateSelector<S> }) =>
-    useXState<S>(this.key, this.initialState, options);
-  Consumer = (props: IConsumerProps<S>) =>
-    XConsumer<S>({ provider: this.key, ...props });
+  useState = (options?: UseStateOptions<S>) => useXState<S>(this.key, this.initialState, options);
+  Consumer = (props: IConsumerProps<S>) => XConsumer<S>({ provider: this.key, ...props });
 }
 
-export const createXStaManager = <S = any>(props: {
-  key: string;
-  initialState: S;
-}) => {
+export const createXStaManager = <S = any>(props: { key: string; initialState: InitialState<S> }) => {
   const { key, initialState } = props;
   return {
     key,
     initialState,
-    getState: () => XSta.get<S>(key) ?? initialState,
-    setState: (nextState: S) => XSta.set<S>(key, nextState),
+    getState: () => XSta.get<S>(key, initialState),
+    setState: (nextState: SetXStateAction<S>) => XSta.set<S>(key, nextState),
     deleteState: () => XSta.delete(key),
-    useState: (options?: { selector?: IXStateSelector<S> }) =>
-      useXState<S>(key, initialState, options),
-    Consumer: (props: IConsumerProps<S>) =>
-      XConsumer<S>({ provider: key, ...props }),
+    useState: (options?: UseStateOptions<S>) => useXState<S>(key, initialState, options),
+    Consumer: (props: IConsumerProps<S>) => XConsumer<S>({ provider: key, ...props }),
   };
 };
 
-export function useXConsumer<S = any>(
-  key: string,
-  selector?: IXStateSelector<S>
-) {
+export const useXConsumer = <S = any>(key: string, selector?: XStateSelector<S>) => {
   return useXState(key, undefined, { selector });
-}
-
-type IConsumerProps<S = any> = {
-  selector?: IXStateSelector<S>;
-  children: ReactNode | ((state: S) => ReactNode);
 };
-export const XConsumer = <S = any>(
-  props: IConsumerProps<S> & { provider: string }
-) => {
+
+type IConsumerProps<S = any> = { selector?: XStateSelector<S>; children: ReactNode | ((state: S) => ReactNode) };
+export const XConsumer = <S = any>(props: IConsumerProps<S> & { provider: string }) => {
   const { provider, selector, children } = props;
   const [state] = useXConsumer<S>(provider, selector);
-  const memoChildrenRef = useMemoSelectorRef<ReactNode, S>({
+  const memoChildrenRef = useMemoWithSelector<ReactNode, S>({
     selector,
-    getCurrent: () => {
-      if (typeof children === "function") {
-        return children(state);
-      }
-      return children;
-    },
     getDeps: () => XSta.get(provider),
-    immediately: true,
+    getCurrent: () => (typeof children === 'function' ? children(state) : children),
   });
+  memoChildrenRef.current.diffChanges();
   return memoChildrenRef.current.state;
 };
 
 let id = 0;
 const newId = () => id++;
+const getValue = <S = any>(data: S | (() => S)) => {
+  return typeof data === 'function' ? (data as CallableFunction)() : data;
+};
 const registerRebuildFn = (key: string, id: string, rebuild: VoidFunction) => {
   if (kStates.has(key)) {
     kStates.get(key)!.rebuilds.set(id, rebuild);
@@ -132,8 +120,7 @@ const registerRebuildFn = (key: string, id: string, rebuild: VoidFunction) => {
     kStates.set(key, { rebuilds: new Map().set(id, rebuild) });
   }
 };
-const unregisterRebuildFn = (key: string, id: string) =>
-  kStates.get(key)?.rebuilds.delete(id);
+const unregisterRebuildFn = (key: string, id: string) => kStates.get(key)?.rebuilds.delete(id);
 
 function useInit<T = any>(fn: () => T, deps?: any[]): T {
   const ref = useRef<{ initialized?: boolean; data?: T; deps?: any[] }>({});
@@ -143,28 +130,20 @@ function useInit<T = any>(fn: () => T, deps?: any[]): T {
   return ref.current.data!;
 }
 
-export function useXProvider<S = any>(
-  key: string,
-  initialState: (() => S) | S
-) {
+export function useXProvider<S = any>(key: string, initialState: InitialState<S>) {
   useInit(() => {
-    if (typeof initialState === "function") {
-      initialState = (initialState as CallableFunction)();
-    }
-    if (!kStates.has(key)) {
-      XSta.set(key, initialState); // only initialize once
-    }
+    !kStates.has(key) && XSta.set(key, initialState); // only initialize once
   }, []);
 }
 
-function useMemoSelectorRef<S = any, D = any>(props: {
+type IUseMemoWithSelector<S = any, D = any> = {
   getCurrent: () => S;
   getDeps: () => D;
   selector?: (deps: D) => any;
   onChange?: (prevState: S, nextState: S) => void;
-  immediately?: boolean; // diff changes immediately
-}) {
-  const { getCurrent, getDeps, selector, onChange, immediately } = props;
+};
+function useMemoWithSelector<S = any, D = any>(props: IUseMemoWithSelector<S, D>) {
+  const { getCurrent, getDeps, selector, onChange } = props;
   const ref = useRef<{
     initialized: boolean;
     state: S;
@@ -184,6 +163,5 @@ function useMemoSelectorRef<S = any, D = any>(props: {
       ref.current.state = nextState;
     }
   };
-  if (immediately) ref.current.diffChanges();
   return ref;
 }
